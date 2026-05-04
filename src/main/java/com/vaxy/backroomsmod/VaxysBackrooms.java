bbff4d3879d1c14945d5e0c1e6314986a9e9006d
Vaxy223 <frantisek2873940@gmail.com>
2026-05-04 21:30:23 +0200

    next version

--- src/main/java/com/vaxy/backroomsmod/VaxysBackrooms.java
+++ src/main/java/com/vaxy/backroomsmod/VaxysBackrooms.java
@@ -55,7 +55,7 @@ public class VaxysBackrooms {
             .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
 
     // Creates a creative tab with the id "vaxysbackrooms:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("backrooms_tab", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.vaxysbackrooms")) //The language key for the title of your CreativeModeTab
             .withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
